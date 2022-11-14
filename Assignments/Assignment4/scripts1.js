70864a3f4952f1127fd1689fd545e8c633cd0aa0
S1ourenco <stephen.lourenco@hotmail.com>
2022-11-14 11:54:29 -0500

    Final

---
+++ Assignments/Assignment4/scripts1.js
@@ -0,0 +1,6 @@
+function darkmode(){
+
+  var element = document.body;
+
+element.classList.toggle ("darkmode");
+} // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_dark_mode //

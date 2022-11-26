d75d3c69b91085a6549fbaff5401ee186bb0455a
S1ourenco <stephen.lourenco@hotmail.com>
2022-11-26 15:37:25 -0500

    Updated Final

--- Assignments/Assignment4/scripts1.js
+++ Assignments/Assignment4/scripts1.js
@@ -6,10 +6,16 @@ element.classList.toggle ("darkmode");
 } // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_dark_mode //
 
 
+function theTime(){
+var date = new Date();
+var currentdate = date.getMonth()+ 1+ "/"+ date.getDate()+ "/"+ date.getFullYear();+"-"+ date.getHours()
+currentdate = currentdate + " - " + date.getHours( )+ ":" + date.getMinutes() + ":" + date.getSeconds();
+document.getElementById('clock').innerHTML = currentdate;
+}
 
-var time = new Date(day,hours,minutes);
+setInterval(theTime, 100);
 
-console.log(time);
+
 
 function showNum(){
 
